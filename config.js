55730ad8814f66bb4762cfe5a3fd820888621ec7
Sergio Soage <51522099+sersoage@users.noreply.github.com>
2019-08-20 14:02:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Blog', // Required
+  author: 'Sergio Soage', // Required
+  description: 'AI R&D',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://www.farpointst.com/',
+    github: 'https://github.com/sersoage',
+    twitter: 'https://twitter.com/Sergio_Soage',
+    instagram: 'https://www.instagram.com/sergio_soage/',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: 'Blog',
+  siteUrl: 'https://www.farpointst.com//',
 };
